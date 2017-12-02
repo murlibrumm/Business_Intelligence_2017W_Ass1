56fa672867ffef8cc29ea1d1fc7909917ca090f5
Wolfgang Weintritt <wolfgang.weintritt@gmail.com>
2017-12-02 21:20:05 +0100

    Add load job, add csv files to load.

--- task1/oltp/TB_CustomerAddress.sql
+++ task1/oltp/TB_CustomerAddress.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS TB_CustomerAddresss (
+CREATE TABLE IF NOT EXISTS TB_CustomerAddress (
 	CustomerID INTEGER NOT NULL,
 	AddressID INTEGER NOT NULL,
+	AddressType VARCHAR(40) NOT NULL,
 	CONSTRAINT PK_CustomerAddress PRIMARY KEY (CustomerID, AddressID),
 	CONSTRAINT FK_Customer_CustomerAddress FOREIGN KEY (CustomerID) REFERENCES TB_Customer (CustomerID),
 	CONSTRAINT FK_Address_CustomerAddress FOREIGN KEY (AddressID) REFERENCES TB_Address (AddressID)
