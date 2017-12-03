701e0c6c09cc1782622eaeb19a78f9eda0f0fa70
Max <maxmanski@live.at>
2017-12-03 20:31:24 +0100

    Implement query B1

---
+++ task3/B1.sql
@@ -0,0 +1,30 @@
+set @num := 0, @cat := '';
+
+SELECT
+	TopProductCategory as 'Top Product Category',
+    rank as 'Product Rank',
+    ProductName as 'Product Name'
+FROM
+(
+	SELECT
+		TopProductCategory,
+        ProductName,
+        Sales,
+        @num := if (@cat = sales.TopProductCategory, @num + 1, 1) as rank,
+        @cat := sales.TopProductCategory as dummy
+	FROM
+		(
+			SELECT
+				p.ProductID as 'ProductID',
+				p.ProductTopCategory AS 'TopProductCategory',
+				p.Name AS 'ProductName',
+				COUNT(*) AS 'Sales'
+			FROM DM_Product p
+			JOIN DM_FactSales f
+				ON p.ProductID = f.ProductID
+			GROUP BY p.ProductID
+			ORDER BY ProductTopCategory ASC, Sales DESC
+		) AS sales
+) AS ranked
+WHERE rank <= 3
+ORDER BY TopProductCategory ASC, rank ASC
