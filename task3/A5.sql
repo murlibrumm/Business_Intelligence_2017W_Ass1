58a63d6e2129f96f8f9825f4c1006c6b80ac49c0
Wolfgang Weintritt <wolfgang.weintritt@gmail.com>
2017-12-03 20:49:42 +0100

    Rename A4->A5, A5->A4

--- task3/A5.sql
+++ task3/A5.sql
@@ -1,33 +1,8 @@
-#NOT WORKING YET
-#TOP 5 Customer
-SELECT cust.name, cust.CustomerID, sum(fact_sales.OrderLineProfit) as sumProfit
+SELECT cust.name as 'Customer Name', SUM(fact_sales.OrderLineProfit) as Profit
 FROM DM_Customer cust
-JOIN DM_FactSales fact_sales on cust.CustomerID = fact_sales.CustomerID
+JOIN DM_FactSales fact_sales on fact_sales.CustomerID = cust.CustomerID
+JOIN DM_Time t on t.Date = fact_sales.OrderDate
+WHERE t.CalendarYear = 2014 AND t.MonthNumberofYear < 7
 GROUP BY cust.CustomerID
-ORDER BY sumProfit desc
-LIMIT 5;
-
-#TOP 4 Products for a customer
-SELECT prod.Name, sum(fact_sales.OrderLineProfit) as sumProfit
-FROM DM_Customer cust
-JOIN DM_FactSales fact_sales on cust.CustomerID = fact_sales.CustomerID
-JOIN DM_Product prod on prod.ProductID = fact_sales.ProductID
-where cust.name = 'Kelsey G. Becker'
-GROUP BY prod.name
-ORDER BY sumProfit desc
-LIMIT 4;
-
-SELECT topCust.Name, topProd.Name
-FROM (SELECT cust.Name, cust.CustomerID, (fact_sales.OrderLineProfit) as sumProfit
-FROM DM_Customer cust
-JOIN DM_FactSales fact_sales on cust.CustomerID = fact_sales.CustomerID
-GROUP BY cust.CustomerID
-ORDER BY sumProfit desc
-LIMIT 5) topCust, (SELECT prod.Name, sum(fact_sales.OrderLineProfit) as sumProfit
-FROM DM_Customer cust
-JOIN DM_FactSales fact_sales on cust.CustomerID = fact_sales.CustomerID
-JOIN DM_Product prod on prod.ProductID = fact_sales.ProductID
-where cust.CustomerID = topCust.CustomerID
-GROUP BY prod.name
-ORDER BY sumProfit desc
-LIMIT 4) topProd;
+ORDER BY Profit desc
+LIMIT 10;
